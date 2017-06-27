2cd435ee2677efea53972bc77ed770c3983ac5f5
Sourangshu Pal <14earcs0158@aryacollege.in>
2017-06-27 23:59:07 +0530

    Minor Changes

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Sourangshu Pal";
+var role = "Web Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+
